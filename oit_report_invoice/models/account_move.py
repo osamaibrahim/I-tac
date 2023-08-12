da8f0999a8c3350269eda1628eae671b438a37e8
Osama Ibrahim <osama.ibrahimac@gmail2.com>
2023-08-12 10:57:40 +0300

    invoice_update

--- oit_report_invoice/models/account_move.py
+++ oit_report_invoice/models/account_move.py
@@ -1,5 +1,7 @@
 from odoo import api, fields, models, _
 from odoo.exceptions import ValidationError
+from datetime import datetime
+
 
 
 class AccountMove(models.Model):
@@ -22,6 +24,26 @@ class AccountMove(models.Model):
         'No Show',
         invisible=True
     )
+    num_word = fields.Char(string="Amount Words:",
+                           compute='_compute_amount_in_word',
+                           ivvisible=True
+                           )
+
+    def _compute_amount_in_word(self):
+        for rec in self:
+            rec.num_word = str(rec.currency_id.amount_to_text(rec.amount_total)) + ' فقط لاغير'
+
+    @api.onchange('date_of_issue')
+    def difference_date(self):
+        for rec in self:
+            if rec.date_of_issue and rec.invoice_date:
+                fmt = '%Y-%m-%d'
+                start_date = str(rec.invoice_date)
+                end_date = str(rec.date_of_issue)
+                d1 = datetime.strptime(start_date, fmt)
+                d2 = datetime.strptime(end_date, fmt)
+                if d2 < d1:
+                    raise ValidationError("Invoice Date must be less than Date of Issue!")
     def compute_project_name(self):
         for rec in self:
             sale = self.env['sale.order'].search(
