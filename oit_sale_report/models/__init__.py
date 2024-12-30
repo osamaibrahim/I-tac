8545d7f699a426160e7cb1c4ad9c9f3d33fcc878
Tarek-alhawy <tarekibrahim.royalconsultants@gmail.com>
2024-12-30 15:21:12 +0200

    remove all modules for update

--- oit_sale_report/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import product_template
-from . import sale_order
-from . import res_config_settings
-from . import hr_department
-from . import res_users
--- oit_sale_report/models/hr_department.py
+++
@@ -1,9 +0,0 @@
-from odoo import _, api, fields, models
-
-
-class HrDepartment(models.Model):
-    _inherit = "hr.department"
-    department_abbreviation = fields.Char(
-        'Department Abbreviation',
-    )
-
