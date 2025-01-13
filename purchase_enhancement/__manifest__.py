24234c547839e77b539cd588c9f2fcec71529884
Tarek-alhawy <tarekibrahim.royalconsultants@gmail.com>
2025-01-13 13:53:16 +0200

    update purchase report

---
+++ purchase_enhancement/__manifest__.py
@@ -0,0 +1,27 @@
+{
+    'name': 'Oit Purchase Enhancement',
+
+    'summary': 'Modifications on Purchase',
+
+    'author': "Tarek Alhawy/OIT Solutions",
+
+    'company': 'OIT Solutions',
+
+    'website': "http://www.oit-solution.com",
+
+    'version': '18.0',
+
+    'category': "OIT Solutions/apps",
+
+    'license': 'AGPL-3',
+
+    'depends': ['account', 'purchase'],
+    'data': [
+        # report
+        'report/purchase_report.xml',
+    ],
+
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
